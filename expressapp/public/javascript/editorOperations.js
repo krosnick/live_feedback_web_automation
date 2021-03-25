7da3d08cee1dbedcd5945d3905ac0703826bda42
Rebecca Krosnick <rkros@umich.edu>
2021-03-24 21:15:34 -0400

    Dropdown menu and left/right selection widgets for terminal now work

--- expressapp/public/javascript/editorOperations.js
+++ expressapp/public/javascript/editorOperations.js
@@ -777,6 +777,77 @@ $(function(){
         // Tell windowSelectionView to click the appropriate button (#showUISnapshots or #hideSnapshots)
         ipcRenderer.sendTo(parseInt(windowSelectionViewID), "showUISnapshots");
     });
+
+    $("body").on("change", "#windowSelectMenu", function(e){
+        const newPageWinID = $(e.target).val();
+        
+        // Update disabled status of left/right buttons appropriately
+        const currentOptionNode = $(`option[value="${newPageWinID}"`);
+
+        // If no prev sibling exists, set disabled to true, otherwise set to false
+        const prevOptionNode = currentOptionNode.prev();
+        if(prevOptionNode.length > 0){
+            $("#left").prop("disabled",false);
+        }else{
+            $("#left").prop("disabled",true);
+        }
+        
+        // If no next sibling exists, set disabled to true, other set to false
+        const nextOptionNode = currentOptionNode.next();
+        if(nextOptionNode.length > 0){
+            $("#right").prop("disabled",false);
+        }else{
+            $("#right").prop("disabled",true);
+        }
+
+        // Hide all .puppeteerTerminal then show this particular one
+        $(".puppeteerTerminal").hide();
+        $(`.puppeteerTerminal[winID=${newPageWinID}]`).show();
+    });
+
+    $("body").on("click", "#left", function(e){
+        // Find currently selected <option>
+        const currentValue = $("#windowSelectMenu").val();
+        const currentOptionNode = $(`option[value="${currentValue}"`);
+        const prevOptionNode = currentOptionNode.prev();
+        
+        // If it has a prev sibling (https://api.jquery.com/prev/), 
+            // change the value of the <select> to that sibling's pageWinID
+            // (https://stackoverflow.com/questions/78932/how-do-i-programmatically-set-the-value-of-a-select-box-element-using-javascript)
+        if(prevOptionNode.length > 0){
+            const newValue = prevOptionNode.attr("value");
+            $("#windowSelectMenu").val(newValue);
+            $("#windowSelectMenu").trigger("change");
+        }
+        // And then if it now has no prev sibling, disable #left
+        if(prevOptionNode.prev().length === 0){
+            $("#left").prop("disabled",true);
+        }
+        // And regardless, enable #right
+        $("#right").prop("disabled",false);
+    });
+
+    $("body").on("click", "#right", function(e){
+        // Find currently selected <option>
+        const currentValue = $("#windowSelectMenu").val();
+        const currentOptionNode = $(`option[value="${currentValue}"`);
+        const nextOptionNode = currentOptionNode.next();
+        
+        // If it has a next sibling (https://api.jquery.com/next/), 
+            // change the value of the <select> to that sibling's pageWinID
+            // (https://stackoverflow.com/questions/78932/how-do-i-programmatically-set-the-value-of-a-select-box-element-using-javascript)
+        if(nextOptionNode.length > 0){
+            const newValue = nextOptionNode.attr("value");
+            $("#windowSelectMenu").val(newValue);
+            $("#windowSelectMenu").trigger("change");
+        }
+        // And then if it now has no next sibling, disable #right
+        if(nextOptionNode.next().length === 0){
+            $("#right").prop("disabled",true);
+        }
+        // And regardless, enable #left
+        $("#left").prop("disabled",false);
+    });
 });
 
 ipcRenderer.on('newWindowAlert', function(event, pageWinID){
